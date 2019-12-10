721a9129d5d3d2e838327740b05c8c5e3484deb8
Erik Kasperson <chefkasperson@gmail.com>
2019-12-10 17:01:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,10 @@
 def hello_t(array)
-  array.each {|name| puts name}
+  if array.length > 0
+    array.each {|name| puts name}
+  else 
+    puts "Hey! No block was given!"
+  end
+  
 end
 
 # call your method here!
